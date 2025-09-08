633eccc33c706fd2b0d37c8d7551cdd03159cd2a
dean-ab <deanabargil@gmail.com>
2025-09-08 09:48:47 +0300

    commit

--- db.ts
+++ db.ts
@@ -1,4 +1,5 @@
 import admin, { firestore } from "firebase-admin";
+import { AddPrefixToKeys } from "firebase-admin/firestore";
 
 export interface User {
   id: string;
@@ -6,6 +7,29 @@ export interface User {
   email: string;
   age: number;
   status: number;
+}
+
+export interface VaultBalance {
+  asset: string;
+  total: string;
+  pending: string;
+  frozen: string;
+  locked: string;
+  staked?: string;
+  pendingRewards?: string;
+  totalStakedCpu?: string;
+  totalStakedNetwork?: string;
+}
+
+type VaultWalletBalance = {
+  balance: string;
+  pendingBalance: string;
+  frozenBalance: string;
+  lockedAmount: string;
+  stakedBalance?: string;
+  totalStakedCpu?: string;
+  totalStakedNetwork?: string;
+  pendingRewards?: string;
 }
 
 class FirestoreDB {
@@ -64,6 +88,98 @@ class FirestoreDB {
         );
       }
     });
+  }
+
+  public async updateVaultWalletBalance(tenantId: string, accountId: string, balance: Partial<VaultBalance> & { asset: string }): Promise<void> {
+    console.log(`In updateVaultWalletBalance, tenantId: ${tenantId}, asset: ${JSON.stringify(balance)}`);
+    await this.updateVaultWalletBalanceDoc(tenantId, accountId, balance);
+    await this.updateVaultWalletBalanceSubCollection(tenantId, accountId, balance);
+    console.log(`Vault wallet balance updated for tenantId: ${tenantId}, accountId: ${accountId}, asset: ${balance.asset}`);
+  }
+
+  private async updateVaultWalletBalanceDoc(tenantId: string, accountId: string, balance: Partial<VaultBalance> & { asset: string }): Promise<void> {
+    console.log(`In updateVaultWalletBalanceDoc, tenantId: ${tenantId}, asset: ${JSON.stringify(balance)}`);
+
+    const update: AddPrefixToKeys<`wallets.${string}`, VaultWalletBalance> = {
+      [`wallets.${balance.asset}.balance`]: balance.total,
+      [`wallets.${balance.asset}.pendingBalance`]: balance.pending,
+      [`wallets.${balance.asset}.frozenBalance`]: balance.frozen,
+      [`wallets.${balance.asset}.lockedAmount`]: balance.locked,
+    };
+
+    if (balance.staked) {
+      update[`wallets.${balance.asset}.stakedBalance`] = balance.staked;
+    }
+
+    if (balance.totalStakedCpu) {
+      update[`wallets.${balance.asset}.totalStakedCpu`] = balance.totalStakedCpu;
+    }
+
+    if (balance.totalStakedNetwork) {
+      update[`wallets.${balance.asset}.totalStakedNetwork`] = balance.totalStakedNetwork;
+    }
+
+    if (balance.pendingRewards) {
+      update[`wallets.${balance.asset}.pendingRewards`] = balance.pendingRewards;
+    }
+
+    await this.db.collection('tenants').doc(tenantId).collection('vaultAccounts').doc(accountId).update(update);
+  }
+
+  private async updateVaultWalletBalanceSubCollection(tenantId: string, accountId: string, balance: Partial<VaultBalance> & { asset: string }): Promise<void> {
+    console.log(`In updateVaultWalletBalanceSubCollection, tenantId: ${tenantId}, asset: ${JSON.stringify(balance)}`);
+    const update: Partial<VaultWalletBalance> = {
+      balance: balance.total,
+      pendingBalance: balance.pending,
+      frozenBalance: balance.frozen,
+      lockedAmount: balance.locked,
+    };
+    if (balance.staked) {
+      update.stakedBalance = balance.staked;
+    }
+    if (balance.totalStakedCpu) {
+      update.totalStakedCpu = balance.totalStakedCpu;
+    }
+    if (balance.totalStakedNetwork) {
+      update.totalStakedNetwork = balance.totalStakedNetwork;
+    }
+    if (balance.pendingRewards) {
+      update.pendingRewards = balance.pendingRewards;
+    }
+    const doc = this.db.collection('tenants').doc(tenantId).collection('vaultAccounts').doc(accountId);
+    const assetWalletRef = doc.collection('wallets').doc(balance.asset);
+    const assetWallet = await assetWalletRef.get();
+    if (assetWallet.exists) {
+      await assetWalletRef.update({ ...update });
+    } else {
+      await assetWalletRef.create(update);
+    }
+  }
+
+  // TODO: Think about moving this to subCollection? 
+  public async getVaultWallets(tenantId: string, vaultAccountId: string): Promise<string[]> {
+    // Add FF guard for sub-collection usage
+    if (true) {
+      // @ts-ignore
+      return this.getVaultWalletsFromSubCollection(tenantId, vaultAccountId);
+    }
+    // @ts-ignore
+    return this.getVaultWalletsFromDoc(tenantId, vaultAccountId);
+  }
+
+  public async getVaultWalletsFromDoc(tenantId: string, vaultAccountId: string): Promise<string[]> {
+    const data = (
+      await this.db.collection("tenants").doc(tenantId).collection('vaultAccounts').doc(vaultAccountId).get()
+    ).data();
+    return Object.keys(data?.wallets);
+  }
+
+  public async getVaultWalletsFromSubCollection(tenantId: string, vaultAccountId: string): Promise<string[]> {
+    const snapshot = await this.db.collection("tenants").doc(tenantId).collection('vaultAccounts').doc(vaultAccountId).collection('wallets').listDocuments();
+    const wallets = snapshot.map(doc => {
+      return doc.id;
+    });
+    return wallets;
   }
 }
 
--- main.ts
+++ main.ts
@@ -3,54 +3,28 @@ import * as admin from "firebase-admin";
 import * as f from "firebase/firestore";
 import cluster from "cluster";
 import { cpus } from "os";
-// import FirestoreDB from "./db";
+import FirestoreDB from "./db";
 import { RealtimeDB } from "./RealtimeDB/RealtimeDB";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
-const CLUSTERED_MODE = false;
+const CLUSTERED_MODE = true;
 
 export enum TestCase {
   StatusUpdate = "StatusUpdate",
   LatestTimestamp = "LatestTimestamp",
+  VaultBalance = "VaultBalance",
 }
 
 const numCPUs = cpus().length;
 
 console.log(`Number of CPUs: ${numCPUs}`);
 
-if (CLUSTERED_MODE) {
-  if (cluster.isPrimary) {
-    console.log(`Primary ${process.pid} is running`);
-
-    // Fork workers
-    for (let i = 0; i < numCPUs; i++) {
-      cluster.fork();
-    }
-
-    cluster.on("exit", (worker) => {
-      console.log(`Worker ${worker.process.pid} died`);
-      // cluster.fork(); // Restart worker
-    });
-  } else {
-    // Worker processes
-    console.log(`Worker ${process.pid} started`);
-    bootstrap()
-      .then(() => {
-        console.log(`Worker ${process.pid} finished`);
-        process.exit(0);
-      })
-      .catch((err) => {
-        console.error(`Worker ${process.pid} error:`, err);
-        process.exit(1);
-      });
-  }
-}
-
 class Main {
   private readonly realtimeDb: RealtimeDB = new RealtimeDB();
+  private readonly firestore: FirestoreDB = new FirestoreDB();
 
   async run(testCases: TestCase[] = []) {
     await this.seed();
@@ -64,34 +38,6 @@ class Main {
   }
 
   async seed() {
-    const user = await this.realtimeDb.collection("users").doc("user1").get();
-    // console.log("Fetched User:", user);
-
-    if (!user) {
-      await this.realtimeDb.collection("users").doc("user1").create({
-        name: "John Doe",
-        age: 30,
-        email: "d@e.com",
-        status: 0,
-      });
-    } else {
-      const data = user.data();
-      console.log("User already exists:", data);
-      await this.realtimeDb.collection("users").doc("user1").set({ status: 0 });
-    }
-
-    try {
-      await this.realtimeDb
-        .collection("users")
-        .doc("user1")
-        .update({ age: 31, status: 0 });
-    } catch (e) {
-      if (isFireStoreError(e)) {
-        console.error(e);
-      } else {
-        throw e;
-      }
-    }
   }
 
   private async runTestCaseScenario(testCase: TestCase) {
@@ -100,6 +46,8 @@ class Main {
         return this.runStatusUpdateTest();
       case TestCase.LatestTimestamp:
         return this.runLatestTimestampTest();
+      case TestCase.VaultBalance:
+        return this.runVaultBalanceTest();
       default:
         throw new Error("Unknown test case");
     }
@@ -136,20 +84,72 @@ class Main {
       const serverNow =
         admin.firestore.Timestamp.fromMillis(timestamp).toMillis();
       console.log(
-        `[${
-          process.pid
-        }] Latest timestamp: ${doc.updateTime?.toMillis()}, current timestamp: ${serverNow}`
+        `[${process.pid}] Latest timestamp: ${doc.updateTime?.toMillis()}, current timestamp: ${serverNow}`
       );
     });
+  }
+
+
+  private async runVaultBalanceTest() {
+    const tenantId = "tenant1";
+    const accountId = "account1";
+    const balance = {
+      asset: "ETH_TEST4",
+      total: "10000",
+      pending: "2000",
+      frozen: "0",
+      locked: "300",
+    };
+
+    try {
+      await this.firestore.updateVaultWalletBalance(tenantId, accountId, balance);
+      console.log(`Vault wallet balance updated for tenantId: ${tenantId}, accountId: ${accountId}`);
+      const walletIds = await this.firestore.getVaultWallets(tenantId, accountId);
+      console.log(`Wallet IDs for tenantId: ${tenantId}, accountId: ${accountId} - ${JSON.stringify(walletIds)}`);
+    } catch (error) {
+      if (isFireStoreError(error)) {
+        console.error(`Firestore error: ${error.code} - ${error.message}`);
+      } else {
+        console.error(`Unexpected error: ${error}`);
+      }
+    }
   }
 }
 
 async function bootstrap() {
-  await new Main().run([TestCase.LatestTimestamp]);
+  await new Main().run([TestCase.VaultBalance]);
 }
 
 bootstrap();
 
 function isFireStoreError(e: any): e is f.FirestoreError {
   return typeof e === "object" && typeof e.code === "number";
+}
+
+if (CLUSTERED_MODE) {
+  if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+
+    // Fork workers
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on("exit", (worker) => {
+      console.log(`Worker ${worker.process.pid} died`);
+      // cluster.fork(); // Restart worker
+    });
+  } else {
+    // Worker processes
+    console.log(`Worker ${process.pid} started`);
+    bootstrap()
+      .then(() => {
+        console.log(`Worker ${process.pid} finished`);
+        process.exit(0);
+      })
+      .catch((err) => {
+        console.error(`Worker ${process.pid} error:`, err);
+        process.exit(1);
+      });
+  }
 }
